12c23f319f280a6fcd90fa77e30fcf2dde3371ff
Liam Naddell <liamnprg@gmail.com>
2017-09-20 12:51:30 -0400

    first commit

---
+++ Makefile
@@ -0,0 +1,13 @@
+PROGNAME=gmn
+CFLAGS=-Wall
+OBJS=main
+all: compile
+
+compile: $(OBJS).o
+	gcc $^ -o $(PROGNAME)
+		
+$(OBJS).o: $(OBJS).c
+	gcc -c $(CFLAGS) $^
+
+clean:
+	rm -f gmn main.o
---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BADINT_MINUS1 "\nSORRY, that was not detected as a valid integer"
+#define PROMPT "\nPlease enter a number, friend:"
+#define PROMPT_GAMEMASTER "\nEnter the number to be guessed:"
+#define GUESS_GREATERTHAN "\nYour guess was too high :'("
+#define GUESS_LESSTHAN "\nYour guess was too low :'("
+#define INT_TOOBIG "\nNumber must be less than or equal to 2000\n"
+#define INT_TOOSMALL "\nNumber must be above -1 or bad integer\n"
+static long getInt();
+
+
+int main() {
+	printf(PROMPT_GAMEMASTER);
+	long p1Input = getInt();
+	long *p2Input = malloc(sizeof(long));
+	if (p1Input == -1) {
+		printf(BADINT_MINUS1);
+		return 1;
+	}
+	if (p1Input < 0) {
+		printf(INT_TOOSMALL);
+		return 1;
+	} else if (p1Input > 2000) {
+		printf(INT_TOOBIG);
+		return 1;
+	}
+	printf("\033c");
+	short i;
+	for ( i = 0; p1Input != *p2Input; i++) {
+		printf(PROMPT);
+		*p2Input = getInt();
+		if (*p2Input == -1) {
+			printf(BADINT_MINUS1); 
+			return 1;
+		}
+		if (*p2Input < p1Input) {
+			printf(GUESS_LESSTHAN); 
+		}
+		if (*p2Input > p1Input) {
+			printf(GUESS_GREATERTHAN);
+		}
+
+	}
+	printf("It only took you %hi attempt(s)\n", i);
+	return 0;
+}
+
+long getInt() {
+	long myInt;
+	long result = scanf("%li", &myInt);
+	if(result < 1) {
+		return -1;
+	}
+	return myInt;
+}
